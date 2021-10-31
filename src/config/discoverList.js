1958a59298687fe74fc278232b2e759246918818
Her-mite <akjdfjjf22@qq.com>
2021-11-01 00:29:21 +0800

    新增发现页导航栏

---
+++ src/config/discoverList.js
@@ -0,0 +1,23 @@
+const discoverList = [
+    {
+        name: "推荐",
+        path: "discover"
+    },{
+        name: "排行榜",
+        path: "mymusic"
+    },{
+        name: "歌单",
+        path: "friend"
+    },{
+        name: "主播电台",
+        path: "market"
+    },{
+        name: "歌手",
+        path: "download"
+    },{
+        name: "新碟上架",
+        path: "download"
+    }
+]
+
+export default discoverList;
--- src/views/Discover/index.tsx
+++ src/views/Discover/index.tsx
@@ -1,15 +1,33 @@
 
-// import React, { useState } from 'react';
-import { Button,  } from 'antd';
+import { useState } from 'react';
+import { Button, Menu } from 'antd';
 // import { Redirect, withRouter, Route, Switch } from "react-router-dom";
+import discoverList from '@/config/discoverList';
+import { MenuInfo } from 'rc-menu/lib/interface';
+import './index.less'
 
 
 //  发现音乐页面展示内容
 const Discover = () => {
+  const [selectKey, setSelectKey] = useState('0');
+
+  // 顶部标签点击后回调
+  const handleClick = (e: MenuInfo, value: { name?: string; path: any; }) => {
+    setSelectKey(e.key);
+  };
   return (
     <div>
-      <Button>??41?</Button>
-    <Button>??41?</Button>
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[selectKey]} className='navMenu'>
+        {
+          discoverList.map((value, index) => {
+            return <Menu.Item className="navMenuItem" key={index} onClick={e => handleClick(e, value)}>
+              {value.name}
+            </Menu.Item>
+          })
+        }
+      </Menu>
+      {/* <Button>??41?</Button>
+      <Button>??41?</Button> */}
     </div>
 
 
