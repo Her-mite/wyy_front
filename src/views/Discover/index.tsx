416c689abef7872223441a86920556fe25688881
Her-mite <akjdfjjf22@qq.com>
2021-11-01 22:47:48 +0800

    轮播图

--- src/views/Discover/index.tsx
+++ src/views/Discover/index.tsx
@@ -1,9 +1,19 @@
 
 import { useState } from 'react';
-import { Button, Menu } from 'antd';
+import { Menu, Carousel } from 'antd';
 // import { Redirect, withRouter, Route, Switch } from "react-router-dom";
 import discoverList from '@/config/discoverList';
 import { MenuInfo } from 'rc-menu/lib/interface';
+
+import { LeftOutlined, RightOutlined } from "@ant-design/icons";
+
+import first_dance from "@/assets/images/carousel/第一支舞.jpeg";
+import such_img from "@/assets/images/carousel/尽管如此.jpeg";
+import maraso_img from "@/assets/images/carousel/马拉松.jpeg";
+import star_img from "@/assets/images/carousel/坠落星辰.jpeg";
+
+
+
 import './index.less'
 
 
@@ -26,12 +36,24 @@ const Discover = () => {
           })
         }
       </Menu>
-      {/* <Button>??41?</Button>
-      <Button>??41?</Button> */}
+
+      <div className="carousel-mode" style={{display:"flex", justifyContent:"center",}}>
+      <Carousel autoplay arrows prevArrow={<LeftOutlined />} nextArrow={<RightOutlined />}  style={{width:"800px"}}>
+          <img style={{width:"100%"}} src={first_dance} alt="first_dance" />
+          <img src={such_img} alt="such_img" />
+          <img src={maraso_img} alt="maraso_img" />
+          <img src={star_img} alt="star_img" />
+      </Carousel>
+      </div>
+      
+
+
     </div>
 
 
   );
 };
+
+
 
 export default Discover;
